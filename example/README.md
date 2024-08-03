f57a3bcbd31752850de47e15c7791a4ca8dbe7ad
Frank Elsinga <frank@elsinga.de>
2024-08-03 18:00:44 +0200

    added a basic example

---
+++ example/README.md
@@ -0,0 +1,8 @@
+# Indoor example
+
+Example how to use the basics of this library and to test the functionality
+
+```sh
+pnpm install
+pnpm run dev
+```
---
+++ example/src/App.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { Map } from "maplibre-gl";
+import { onMounted, ref } from "vue";
+import {
+  addIndoorTo,
+  IndoorControl,
+  IndoorMap,
+  MapGLWithIndoor,
+} from "maplibre-gl-indoor";
+onMounted(maybeCreateMap);
+
+function maybeCreateMap() {
+  if (document.getElementById("map")) {
+    setTimeout(createMap, 0);
+  } else {
+    setTimeout(maybeCreateMap, 1000);
+  }
+}
+const indoorMap = ref<MapGLWithIndoor | null>(null);
+
+async function createMap() {
+  const baseMap = new Map({
+    style: "https://nav.tum.de/maps/styles/osm-liberty/style.json",
+    center: [8.3909479, 49.0332499],
+    zoom: 17.5,
+    container: "map",
+    hash: true,
+    antialias: true,
+  });
+  const map = addIndoorTo(baseMap);
+  const geojson = await (await fetch("/example.geojson")).json();
+  let indoorJSONMap = IndoorMap.fromGeojson(geojson);
+  await map.indoor.addMap(indoorJSONMap);
+
+  // Add the specific control
+  map.addControl(new IndoorControl(), "bottom-left");
+  indoorMap.value = map;
+}
+</script>
+
+<template>
+  <div id="map"></div>
+</template>
+
+<style>
+#map {
+  min-height: 100vh;
+  min-width: 100vw;
+}
+</style>
