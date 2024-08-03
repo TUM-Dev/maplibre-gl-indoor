db829e70880088b0f2016b802c1e0b0ff9f9a8b5
Frank Elsinga <frank@elsinga.de>
2024-08-03 23:31:00 +0200

    improved how the example works for debugging purposes

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build:{
+    sourcemap: true,
+  },
   plugins: [vue()],
+  server:{
+    open: true,
+  }
 });
