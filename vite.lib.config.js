d122a8a5ca61a0fab2dc9b79a2a48c882220c3ef
Frank Elsinga <frank@elsinga.de>
2024-08-03 16:43:51 +0200

    experimented with different export semantics

--- vite.lib.config.js
+++ vite.lib.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
         lib: {
             entry: resolve(__dirname, "src/index.ts"),
             name: "mapgl_indoor",
-            fileName: (format) => `map-gl-indoor.${format}.js`,
+            fileName: (format) => `maplibre-gl-indoor.${format}.js`,
             formats: ["es", "umd", "cjs"],
         },
         outDir: resolve(__dirname, "dist"),
